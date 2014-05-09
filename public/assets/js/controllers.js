195c7b37feb0e9bb74d71faed3d37f80ecd3b579
Aaron <aaron.rosenbaum@gmail.com>
2014-05-09 15:51:55 -0400

    Fixed a JS Error

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -1,19 +1,19 @@
 "use strict";
 var app = angular.module("ff",[]);
+
 app.factory("MoviesFactory",function($http){
-function getMovies(callback){
-$http.get("/movies").success(callback);
-}
-function getMovie(id,callback){
-$http.get("/movies/"+id).success(callback);
-}
-return {
-getMovies: getMovies,
-getMovie: getMovie
-};
+  return {
+    getMovies: function(){
+      return $http.get("/movies");
+    },
+    getMovie: function(id){
+      return $http.get("/movies/"+id);
+    }
+  }
 });
+
 app.controller("HomeControlller",function($scope,MoviesFactory){
-MoviesFactory.getMovies(function(data){
-$scope.movies = data;
-});
+  MoviesFactory.getMovies(function(data){
+    $scope.movies = data;
+  });
 });
