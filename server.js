b3718a2edff63ff6614364dca79cb87536513239
Aaron <aaron.rosenbaum@gmail.com>
2014-05-09 14:16:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser());
 app.use(methodOverride());
 
 app.get('/',function(req,res){
-  res.sendfile("/public/index.html");
+  res.sendfile("/assets/index.html");
 });
 app.get('/movies',function(req,res){
   // http://node-fast-forward-107599.use1-2.nitrousbox.com/movies
