01f83ca83309bad0e014e85ef4a5d694760095a9
Aaron <aaron.rosenbaum@gmail.com>
2014-05-09 15:06:41 -0400

    Update controllers.js

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -2,10 +2,10 @@
 var app = angular.module("ff",[]);
 app.factory("MoviesFactory",function($http){
 function getMovies(){
-return $http.get("/movies");
+$http.get("/movies");
 }
 function getMovie(id){
-return $http.get("/movies/"+id);
+$http.get("/movies/"+id);
 }
 return {
 getMovies: getMovies,
@@ -13,5 +13,7 @@ getMovie: getMovie
 };
 });
 app.controller("HomeControlller",function($scope,MoviesFactory){
-$scope.movies = MoviesFactory.getMovies();
+$scope.movies = MoviesFactory.getMovies().success(data){
+return data;
+};
 });
