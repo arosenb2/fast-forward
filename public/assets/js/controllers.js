497e20736f040bf6c00883ebc9bb84b8b63ba517
Aaron <aaron.rosenbaum@gmail.com>
2014-05-09 14:55:48 -0400

    Started base app, added Movies Factory and started Home Controller

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -1,1 +1,17 @@
 "use strict";
+var app = angular.module("ff",[]);
+app.factory("MoviesFactory",function($http){
+function getMovies(){
+return $http.get("/movies");
+}
+function getMovie(id){
+return $http.get("/movies/"+id);
+}
+return {
+getMovies: getMovies,
+getMovie: getMovie
+};
+});
+app.controller("HomeControlller",function($scope,MoviesFactory){
+$scope.movies = MoviesFactory.getMovies();
+});
